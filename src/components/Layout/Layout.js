11c63b702b69ec5cbd90c3019e5f8a7dfcf3fa19
webdevabhi <abhishek.patel185@gmail.com>
2018-05-31 14:23:50 +0530

    Buger Builder Container

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
+import classes from './Layout.css';
 import Aux from '../../hoc/Aux';
 
 const layout = ( props ) => (
     <Aux>
         <div>Toolbar, SideDrawer, BackDrop</div>
-        <main>
+        <main className={classes.Content}>
             {props.children}
         </main>
     </Aux>
