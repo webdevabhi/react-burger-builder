80f9f7e669aac20942575f51af2f46118c08dbed
webdevabhi <abhishek.patel185@gmail.com>
2018-06-27 17:56:46 +0530

    Navigation Links

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -6,8 +6,8 @@ import classes from './NavigationItems.css';
 const navigationItems = () => {
     return (
         <ul className={classes.NavigationItems}>
-            <NavigationItem link="/" active>Burger Builder</NavigationItem>
-            <NavigationItem link="/">Checkout</NavigationItem>
+            <NavigationItem link="/" exact>Burger Builder</NavigationItem>
+            <NavigationItem link="/orders">Orders</NavigationItem>
         </ul>
     );
 };
