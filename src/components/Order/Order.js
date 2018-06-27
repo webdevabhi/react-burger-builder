e6128b674e684c241636b440b99cc883ff88ec95
webdevabhi <abhishek.patel185@gmail.com>
2018-06-27 18:19:13 +0530

    Looping Orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,11 +2,32 @@ import React from 'react';
 
 import classes from "./Order.css";
 
-const order = () => {
+const order = (props) => {
+
+    const ingredients = [];
+    for(let ingred in props.ingredients) {
+        ingredients.push({
+            name: ingred,
+            amount: props.ingredients[ingred]
+        });
+    }
+
+    const ingredientOutput = ingredients.map(ig => {
+        return <span 
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                border: '1px solid #ccc',
+                padding: '5px'
+            }}
+            key={ig.name}>{ig.name} ({ig.amount})</span>
+    })
+
     return (
         <div className={classes.Order}>
-            <p>Ingredients: Salad (1)</p>
-            <p>Price: <strong>USD 4.5</strong></p>
+            <p>Ingredients: {ingredientOutput}</p>
+            <p>Price: <strong>USD {Number.parseFloat(props.price).toFixed(2)}</strong></p>
         </div>
     );
 };
