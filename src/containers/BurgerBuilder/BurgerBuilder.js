63c919cd6af1a07a335696427f5f97f2219990f0
webdevabhi <abhishek.patel185@gmail.com>
2018-06-27 17:33:43 +0530

    Order Submission

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,35 +100,13 @@ class BurgerBuilder extends Component {
 
   purchaseContinueHandler = () => {
     // alert("You Continue !");
-    /* this.setState({loading: true});
-    const order = {
-      ingredients : this.state.ingredients,
-      price: this.state.totalPrice,
-      customer : {
-        name: 'Abhishek Patel',
-        adress: {
-          street: 'test street 1',
-          zipCode: '460001',
-          country: 'India'
-        },
-        email: 'test@firebase.com'
-      },
-      deliveryMethod: 'fastest'
-    }
-    axios.post('/orders.json', order)
-      .then(res => { 
-        this.setState({ loading: false, purchasing: false }); 
-      })
-      .catch(err => { 
-        console.error(err); 
-        this.setState({ loading: false, purchasing: false }); 
-      }); */
     const queryParams = [];
     for(let i in this.state.ingredients) {
       queryParams.push(
         encodeURIComponent(i) + "=" + encodeURIComponent(this.state.ingredients[i])
       );
     }
+    queryParams.push('price=' + this.state.totalPrice)
     const queryString = queryParams.join('&');
     this.props.history.push({
       pathname: '/checkout',
