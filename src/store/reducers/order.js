9e69c34bd4a3deafcb3f622644c0e2aa207c573d
webdevabhi <abhishek.patel185@gmail.com>
2018-07-12 16:03:43 +0530

    Fetching Orders (via Redux)

--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -38,6 +38,25 @@ const reducer = (state = initialState, action) => {
                 ...state,
                 loading: false,
             };
+
+        case actionTypes.FETCH_ORDERS_START:
+            return {
+                ...state,
+                loading: true,
+            };
+
+        case actionTypes.FETCH_ORDERS_SUCCESS:
+            return {
+                ...state,
+                loading: false,
+                orders: action.orders
+            };
+
+        case actionTypes.FETCH_ORDERS_FAIL:
+            return {
+                ...state,
+                loading: false,
+            };
         
         default:
             return state;
