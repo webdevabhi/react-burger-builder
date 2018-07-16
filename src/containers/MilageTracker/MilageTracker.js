63cd6ccbffad48d22fc99ba14c4ecd205933167f
webdevabhi <abhishek.patel185@gmail.com>
2018-07-16 14:11:54 +0530

    started working on milage-tracker

---
+++ src/containers/MilageTracker/MilageTracker.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MilageTracker extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello Milage tracker</h1>
+            </div>
+        );
+    }
+}
+
+export default MilageTracker;
