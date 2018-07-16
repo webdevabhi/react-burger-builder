810d77928ecb303975ae6f0cbb502688b9c491c8
webdevabhi <abhishek.patel185@gmail.com>
2018-07-16 15:36:06 +0530

    removed milage tracker from app

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Orders from './containers/Orders/Orders';
-import MilageTracker from './containers/MilageTracker/MilageTracker';
 
 class App extends Component {
   render() {
@@ -15,7 +14,6 @@ class App extends Component {
           <Switch>
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
-            <Route path="/milage-tracker" component={MilageTracker} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
