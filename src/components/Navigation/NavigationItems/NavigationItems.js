9d1db905e40e374dd2242aefba7493d9ce25854b
webdevabhi <abhishek.patel185@gmail.com>
2018-07-17 22:32:14 +0530

    Adding a Logout Link

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -9,7 +9,7 @@ const navigationItems = (props) => {
             <NavigationItem link="/" exact>Burger Builder</NavigationItem>
             <NavigationItem link="/orders">Orders</NavigationItem>
             { props.isAuthenticate
-                ? <NavigationItem link="/auth">Logout</NavigationItem>
+                ? <NavigationItem link="/logout">Logout</NavigationItem>
                 : <NavigationItem link="/auth">Login</NavigationItem> }
         </ul>
     );
