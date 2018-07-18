e13911e5c40809a57f4f3770b689166ecec45096
webdevabhi <abhishek.patel185@gmail.com>
2018-07-18 18:25:38 +0530

    Adding Lazy Loading

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
+import assyncComponent from "./hoc/assyncComponent/assyncComponent";
 
 import Layout from './components/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import Checkout from './containers/Checkout/Checkout';
-import Orders from './containers/Orders/Orders';
-import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
 import * as actions from "./store/actions/index";
+
+const assyncCheckout = assyncComponent(() => {
+  return import('./containers/Checkout/Checkout');
+});
+
+const assyncOrders = assyncComponent(() => {
+  return import('./containers/Orders/Orders');
+});
+
+const assyncAuth = assyncComponent(() => {
+  return import("./containers/Auth/Auth");
+});
 
 class App extends Component {
 
@@ -19,7 +29,7 @@ class App extends Component {
   render() {
     let routes = (
       <Switch>
-        <Route path="/auth" component={Auth} />
+        <Route path="/auth" component={assyncAuth} />
         <Route path="/" exact component={BurgerBuilder} />
         <Redirect to="/" />
       </Switch>
@@ -28,10 +38,10 @@ class App extends Component {
     if (this.props.isAuthenticated) {
       routes = (
         <Switch>
-          <Route path="/checkout" component={Checkout} />
-          <Route path="/orders" component={Orders} />
+          <Route path="/checkout" component={assyncCheckout} />
+          <Route path="/orders" component={assyncOrders} />
           <Route path="/logout" component={Logout} />
-          <Route path="/auth" component={Auth} />
+          <Route path="/auth" component={assyncAuth} />
           <Route path="/" exact component={BurgerBuilder} />
           <Redirect to="/" />
         </Switch>
