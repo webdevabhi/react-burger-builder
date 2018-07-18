dae5bc47c3f987eee7287fd957597d6ad6a25ffa
webdevabhi <abhishek.patel185@gmail.com>
2018-07-18 12:15:35 +0530

    redirect users to checkout page

--- src/store/reducers/auth.js
+++ src/store/reducers/auth.js
@@ -6,7 +6,8 @@ const intialState = {
     token: null,
     userId: null,
     error: null,
-    loading: false
+    loading: false,
+    authRedirectPath: '/'
 };
 
 const authStart = (state, action) => {
@@ -36,6 +37,10 @@ const authLogout = (state, action) => {
     });
 }
 
+const setAuthRedirectPath = (state, action) => {
+    return updateObject(state, { authRedirectPath: action.path });
+}
+
 const reducer = (state = intialState, action) => {
     switch (action.type) {
         case actionTypes.AUTH_START:
@@ -49,6 +54,9 @@ const reducer = (state = intialState, action) => {
 
         case actionTypes.AUTH_LOGOUT:
             return authLogout(state, action);
+
+        case actionTypes.SET_AUTH_REDIRECT_PATH:
+            return setAuthRedirectPath(state, action);
     
         default:
             return state;
